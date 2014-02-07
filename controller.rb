43cfa9bfb86d13ff0c6464a42e0f127ce34d1c64
Torey Hickman <torey@toreyhickman.com>
2014-02-07 16:17:12 -0600

    Code Review

--- controller.rb
+++ controller.rb
@@ -6,6 +6,22 @@ class Controller
 
   attr_accessor :flashcards, :card
   def initialize(file)
+    # There are a few issues here, in my opinion.
+    # (1) The name @flashcards doesn't represent its value.  It doesn't hold
+    # multiple flashcards, it holds a Deck (@deck).  (2) The controller is
+    # currently reaching into the @flashcards object to get a Card object; it knows that
+    # @flashcards responds to a method #deck and that the return value responds to
+    # a method [].  Remember the Law of Demeter.  Instead, try @flashcards.get_card.
+    # (3) All that said, I don't believe there's a need for the controller to hold a specific card
+    # in one of its instance variables.  I would not have @card.  In any case, you don't need to
+    # assign its value in the initialize method.  That's the first thing the play method does.
+    # (4) Lines 22 and 23 could be extracted into another method, maybe begin_play.
+    #
+    # def initialize(file)
+    #   @deck = Deck.new(file)
+    #   begin_play
+    # end
+
     @flashcards = Model.new(file)
     @card = flashcards.deck[0]
     welcome_message
@@ -13,6 +29,9 @@ class Controller
   end
 
   def pull_card
+    # Again, instead of reaching into the deck to get a card, the deck should return a card
+    # when sent the right message (e.g., get_card).  Also, I wouldn't set @card, just return a Card.
+
     self.card = flashcards.deck[0] #pick the card to display the question
   end
 
@@ -21,6 +40,10 @@ class Controller
   end
 
   def play
+    # I would set a local variable card equal to pull_card.  Again, the Controller is reaching into the deck object.
+    # It's shuffling the deck's cards.  The controller should tell the deck to shuffle, and the deck should implement
+    # that itself.  But, all you're really doing is getting a random card.  The pull_card method could call get_card
+    # on the deck.  The deck could respond by calling Array#sample on its cards.
     pull_card
     flashcards.deck.shuffle!
     display_guess_ask(card)
--- model.rb
+++ model.rb
@@ -1,14 +1,26 @@
-class Model
+class Model # A better name would be Deck
   attr_accessor :deck
   attr_reader :file
 
   def initialize(file)
+    # First, @deck should be renamed to something like @cards; that's what it holds.
+    # Second, there's no need to save the filename to an instance variable, and
+    # @deck doesn't need to be set to an empty array.  Just set it to the value of parser.
+    #
+    # def initialize(file)
+    #   @cards = parser(file)
+    # end
+
     @file = file
     @deck = []
     parser
   end
 
   def parser
+    # The name could be improved, maybe something like parse_file_to_cards,
+    # if you want it coupled to the Card model.
+    # This method could be moved to a parsing module.
+
     File.open(file).each_slice(3) { |line| self.deck << Card.new(line) }
   end
 end
@@ -17,6 +29,9 @@ class Card
   attr_reader :question, :answer
 
   def initialize(attributes)
+    # The attributes are coming in as an array.  Therefore, any object instantiating a
+    # Card object has to know how to order the elements.
+
     @question = attributes[0].chomp
     @answer = attributes[1].chomp
   end
